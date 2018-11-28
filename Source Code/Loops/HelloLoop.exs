55a564976edf0c16f648049030b90ff0f7d98db1
Nick Lai <nicklai802@gmail.com>
2018-11-28 00:42:39 -0800

    Add files via upload

--- Source Code/Loops/HelloLoop.exs
+++ Source Code/Loops/HelloLoop.exs
@@ -1,12 +1,15 @@
+#prints "Hello" to the console 10 times, each in different lines
 defmodule Loop do
+  #prints the message for the final time in the loop based on parameters
    def print_multiple_times(msg, n) when n <= 1 do
       IO.puts msg
    end
-
+   #recursively prints a message to the console on a new line and decrements the counter
    def print_multiple_times(msg, n) do
       IO.puts msg
       print_multiple_times(msg, n - 1)
    end
 end
 
+#calls the loop module with parameters
 Loop.print_multiple_times("Hello", 10)
